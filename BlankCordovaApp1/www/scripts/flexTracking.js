33a9101f88b839a9c5e035a6a3a198f16523d9e7
Justin Largent <jmblargent@gmail.com>
2017-03-31 00:29:38 -0500

    It's pretty much completely working now.

--- BlankCordovaApp1/www/scripts/flexTracking.js
+++ BlankCordovaApp1/www/scripts/flexTracking.js
@@ -1,3 +1,19 @@
 ﻿(function () {
-    var flexBalance = document.getElementById("mealPlanSelect").nodeValue;
+    var temp;
+    var flexBalance = localStorage.mealPlan;
+    document.getElementById("flexBalance").innerHTML = flexBalance;
+
+    document.getElementById("flexChangeButton").addEventListener('click', function () {
+        document.getElementById("flexBalanceChange").submit();
+        temp = document.getElementById("flexBalanceChange")['flexChange'].value;
+        if (localStorage.mealPlan - temp <= 0)
+            localStorage.mealPlan = 0;
+        else
+            localStorage.mealPlan = localStorage.mealPlan - temp;
+        document.getElementById("flexBalance").innerHTML = flexBalance;
+    });
+
+    document.getElementById("backToStart").addEventListener('click', function () {
+        window.location.assign("mealPlanSelectPage.html");
+    });
 })();
--- BlankCordovaApp1/www/scripts/mealPlanSelect.js
+++ BlankCordovaApp1/www/scripts/mealPlanSelect.js
@@ -1,5 +1,10 @@
-﻿(function () {
+﻿var mealPlan;
+(function () {
     document.getElementById('mealPlanSelectButton').addEventListener('click', function () {
+        document.getElementById('flexPlanSelect').submit();
+        mealPlan = document.getElementById('flexPlanSelect')['flexPlan'].value;
+        localStorage.mealPlan = mealPlan;
+        console.log(mealPlan);
         window.location.assign("flexTracking.html");
     });
 })();
