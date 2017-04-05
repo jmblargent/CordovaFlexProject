af8761b1aabca7b78c4c4bd0ce82831005d41ab5
Xeavior <xeavior@gmail.com>
2017-04-04 22:02:32 -0500

    Added more Ticket Balance Functionality
    started adding ticket balance with flex balance.

--- BlankCordovaApp1/www/scripts/flexTracking.js
+++ BlankCordovaApp1/www/scripts/flexTracking.js
@@ -1,17 +1,30 @@
 ﻿(function () {
     var temp;
     var flexBalance = localStorage.flexMoney;
+    var ticketBalance = localStorage.ticketCount;
+    var monthlyFlag = localStorage.monthlyFlag;
     document.getElementById("flexBalance").innerHTML = flexBalance;
+    document.getElementById("ticketBalance").innerHTML = ticketBalance;
 
     document.getElementById("flexChangeButton").addEventListener('click', function () {
         document.getElementById("flexBalanceChange").submit();
+
         temp = document.getElementById("flexBalanceChange")['flexChange'].value;
         if (localStorage.flexMoney - Math.abs(temp) <= 0)
             localStorage.flexMoney = 0;
         else
-            localStorage.mealPlan = localStorage.mealPlan - Math.abs(temp);
+            localStorage.flexMoney = localStorage.flexMoney - Math.abs(temp);
         document.getElementById("flexBalance").innerHTML = flexBalance;
     });
+
+    document.getElementById("ticketChangeButton").addEventListener('click', function () {
+
+        ticketBalance = ticketBalance - 1;
+        localStorage.ticketCount = ticketBalance;
+
+        document.getElementById("ticketBalance").innerHTML = ticketBalance;
+    });
+
 
     document.getElementById("backToStart").addEventListener('click', function () {
         window.location.assign("mealPlanSelectPage.html");
--- BlankCordovaApp1/www/scripts/mealPlanSelect.js
+++ BlankCordovaApp1/www/scripts/mealPlanSelect.js
@@ -1,24 +1,26 @@
-﻿var flexMoney;
-var monthlyFlag;
-(function () {
+﻿(function () {
+
+    var flexMoney;
+    var monthlyFlag = false;
+
     document.getElementById('mealPlanSelectButton').addEventListener('click', function () {
         document.getElementById('flexPlanSelect').submit();
-        flexMoney = document.getElementById('flexPlanSelect')['flexPlan'].value;
-        localStorage.flexMoney = flexMoney;
-        console.log(flexMoney);
 
-        switch(flexMoney){
+        localStorage.flexMoney = document.getElementById('flexPlanSelect')['flexPlan'].value;
+        alert(localStorage.flexMoney);
 
-            case 400:
+        switch(localStorage.flexMoney){
+
+            case "400":
                 localStorage.ticketCount = 10;
                 break;
-            case 150:
+            case "150":
                 localStorage.ticketCount = 15;
                 break;
-            case 35:
+            case "35":
                 localStorage.ticketCount = 19;
                 break;
-            case 350:
+            case "350":
                 localStorage.ticketCount = 100;
 
                 //if there is a way to handle allowing them to reset their weekly ticket
@@ -27,6 +29,7 @@ var monthlyFlag;
                 break;
         }
 
+        localStorage.monthlyFlag = monthlyFlag;
         window.location.assign("flexTracking.html");
     });
 })();
